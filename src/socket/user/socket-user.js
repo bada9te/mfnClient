b372da08a9bfd65d7d7788ca9f44f86718212eb4
Bohdan <bogdantelep@gmail.com>
2023-07-15 17:16:15 -0700

    temporar changes

--- src/socket/user/socket-user.js
+++ src/socket/user/socket-user.js
@@ -3,7 +3,6 @@ import { io } from "socket.io-client";
 const SERVER_BASE = process.env.REACT_APP_SERVER_BASE;
 
 // socket
-console.log(SERVER_BASE)
 const userSocket = io(SERVER_BASE.endsWith('/') ? SERVER_BASE : `${SERVER_BASE}/`, {
     transports : ['websocket']
 });
