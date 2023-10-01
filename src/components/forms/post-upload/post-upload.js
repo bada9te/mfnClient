696bbe6b4e6297ac380596d06991125ab20d89ed
Bohdan <bogdantelep@gmail.com>
2023-10-01 23:47:51 +0300

    updating visuals

--- src/components/forms/post-upload/post-upload.js
+++ src/components/forms/post-upload/post-upload.js
@@ -1,8 +1,6 @@
 import { useForm } from "react-hook-form";
 import { Stack, Card, Typography, CardContent, FormControlLabel, Button, Box, TextField, Checkbox, FormGroup } from "@mui/material";
 import * as Alert from "../../alerts/alerts";
-import PostItem from "../../common/post-item/post-item";
-import ImageCropperModal from "../../modals/image-cropper-modal/image-cropper-modal";
 import { useDispatch, useSelector } from "react-redux";
 import { savePostFile, setAudio, setAudioTitle, setCommentsAllowed, setDescription, setDownloadsAllowed, setImageTitle, setPicture, setTitle, uploadPost } from "./postUploadFormSlice";
 import { setImageType, setIsShowing as setCropModalIsShowing } from "../../modals/image-cropper-modal/imageCropperModalSlice";
@@ -13,15 +11,9 @@ import { unwrapResult } from "@reduxjs/toolkit";
 
 const PostUploadForm = (props)=> {
     const { register, handleSubmit, formState: { errors }, reset } = useForm();
-    const title = useSelector(state => state.postUploadForm.title);
-    const description = useSelector(state => state.postUploadForm.description);
     const audioTitle = useSelector(state => state.postUploadForm.audioTitle);
     const imageTitle = useSelector(state => state.postUploadForm.imageTitle);
     const picture = useSelector(state => state.postUploadForm.picture);
-    const audio = useSelector(state => state.postUploadForm.audio);
-    const currentUser = useSelector(state => state?.base?.user);
-    const locations = useSelector(state => state?.base?.locations);
-    const isShowing = useSelector(state => state.imageCropperModal.isShowing);
     const commentsAllowed = useSelector(state => state.postUploadForm.commentsAllowed);
     const downloadsAllowed = useSelector(state => state.postUploadForm.downloadsAllowed);
     const theme = useSelector(state => state.base.theme);
@@ -59,12 +51,7 @@ const PostUploadForm = (props)=> {
         }, { theme });  
     }
 
-    const handleImageCropModalClose = (value, picture) => {
-        dispatch(setCropModalIsShowing(value));
-        if (picture !== null) {
-            dispatch(setPicture(picture));
-        }
-    }
+    
     
     // handlers
     const handlePicture = (file) => { 
@@ -89,146 +76,94 @@ const PostUploadForm = (props)=> {
     }, [])
 
 
-    return(
-        <>  
-            {
-                isShowing
-                ? 
-                <ImageCropperModal 
-                    show={isShowing} 
-                    handleImageCropModalClose={handleImageCropModalClose} 
-                    image={picture} 
-                    what={'post-image'}
-                />
-                :
-                null
-            }
-            <Box sx={{ width: '100%' }}>
-                <Stack spacing={{ xs: 1, sm: 2 }} sx={{display: 'flex', justifyContent: 'space-around', alignItems: 'center'}} direction="row" useFlexGap flexWrap="wrap">
-                    <Card sx={{width: '40rem', height: 'fit-content', boxShadow: 3, borderRadius: 5}}>
-                        <Typography gutterBottom variant="h4" component="div" sx={{display: 'flex', justifyContent: 'center', pt: 3, mb: 0}}>
-                            Your new post
-                        </Typography>
-                        <CardContent>
-                            <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{margin: 1}}>
-                                <TextField
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    id="title"
-                                    label="Title"
-                                    name="title"
-                                    error={Boolean(errors.Title)}
-                                    helperText={errors.Title && "Title must be from 4 to 10 characters"}
-                                    onInput={(e) => dispatch(setTitle(e.target.value))}
-                                    {...register("Title", {
-                                        maxLength: 10,
-                                        minLength: 4,
-                                        required: true,
-                                    })}
-                                />
-                                <TextField
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    id="description"
-                                    label="Short description"
-                                    name="description"
-                                    error={Boolean(errors.ShortDesc)}
-                                    helperText={errors.ShortDesc && "Short description must be from 4 to 20 characters"}
-                                    onInput={(e) => dispatch(setDescription(e.target.value))}
-                                    {...register("ShortDesc", {
-                                        maxLength: 40,
-                                        minLength: 4,
-                                        required: true,
-                                    })}
-                                />
-                                <FormGroup sx={{my: 2}}>
-                                    <Button variant="outlined" component="label">
-                                        {imageTitle}
-                                        <input type="file" hidden 
-                                            onInput={e => handlePicture(e.target.files[0] || null)}
-                                            {...register("Image", {
-                                                required: true,
-                                            })} 
-                                        />
-                                    </Button>
-                                </FormGroup>
-                                <FormGroup sx={{my: 2}}>
-                                    <Button variant="outlined" component="label">
-                                        {audioTitle}
-                                        <input type="file" hidden 
-                                            onInput={e => handleAudio(e.target.files[0] || null)}
-                                            {...register("Audio", {
-                                                required: true,
-                                            })} 
-                                        />
-                                    </Button>
-                                </FormGroup>
-                                <FormGroup>
-                                    <FormControlLabel
-                                        control={
-                                            <Checkbox color="primary" {...register("AllowComments", {})} 
-                                                checked={commentsAllowed} 
-                                                onChange={(e) => dispatch(setCommentsAllowed(e.target.checked))}
-                                            />
-                                        }
-                                        label="Allow comments"
-                                    />
-                                </FormGroup>
-                                <FormGroup>
-                                    <FormControlLabel
-                                        control={
-                                            <Checkbox color="primary" {...register("AllowDownloads", {})}
-                                                checked={downloadsAllowed} 
-                                                onChange={(e) => dispatch(setDownloadsAllowed(e.target.checked))}
-                                            />
-                                        }
-                                        label="Allow downloads"
-                                    />
-                                </FormGroup>
-
-                                <Button
-                                    type="submit"
-                                    fullWidth
-                                    variant="contained"
-                                    sx={{ mt: 3, mb: 2, boxShadow: 10 }}
-                                >
-                                    Upload
-                                </Button>
-                            </Box>
-                        </CardContent>
-                    </Card>
-
-
-                    <PostItem
-                        base={{
-                            owner: {
-                                _id: currentUser?._id,
-                                nick: currentUser?.nick,
-                            },
-                            ownerAvatar: `${locations?.images}/${currentUser?.avatar}`,
-                            createdAt: 'now',
-                            title,
-                            description,
-                            img: picture,
-                            audio,
-                            likedBy: [],
-                            savedBy: [],
-                            comments: [],
-                        }}
-                        addons={{
-                            commentsAllowed,
-                            downloadsAllowed,
-                            status: "upload",
-                            profileLinkAccessable: false,
-                        }}
-                        id={null}
-                        
+    return(          
+        <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{margin: 1}}>
+            <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="title"
+                label="Title"
+                name="title"
+                error={Boolean(errors.Title)}
+                helperText={errors.Title && "Title must be from 4 to 10 characters"}
+                onInput={(e) => dispatch(setTitle(e.target.value))}
+                {...register("Title", {
+                    maxLength: 10,
+                    minLength: 4,
+                    required: true,
+                })}
+            />
+            <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="description"
+                label="Short description"
+                name="description"
+                error={Boolean(errors.ShortDesc)}
+                helperText={errors.ShortDesc && "Short description must be from 4 to 20 characters"}
+                onInput={(e) => dispatch(setDescription(e.target.value))}
+                {...register("ShortDesc", {
+                    maxLength: 40,
+                    minLength: 4,
+                    required: true,
+                })}
+            />
+            <FormGroup sx={{my: 2}}>
+                <Button variant="outlined" component="label">
+                    {imageTitle}
+                    <input type="file" hidden 
+                        onInput={e => handlePicture(e.target.files[0] || null)}
+                        {...register("Image", {
+                            required: true,
+                        })} 
                     />
-                </Stack>
-            </Box>
-        </>
+                </Button>
+            </FormGroup>
+            <FormGroup sx={{my: 2}}>
+                <Button variant="outlined" component="label">
+                    {audioTitle}
+                    <input type="file" hidden 
+                        onInput={e => handleAudio(e.target.files[0] || null)}
+                        {...register("Audio", {
+                            required: true,
+                        })} 
+                    />
+                </Button>
+            </FormGroup>
+            <FormGroup>
+                <FormControlLabel
+                    control={
+                        <Checkbox color="primary" {...register("AllowComments", {})} 
+                            checked={commentsAllowed} 
+                            onChange={(e) => dispatch(setCommentsAllowed(e.target.checked))}
+                        />
+                    }
+                    label="Allow comments"
+                />
+            </FormGroup>
+            <FormGroup>
+                <FormControlLabel
+                    control={
+                        <Checkbox color="primary" {...register("AllowDownloads", {})}
+                            checked={downloadsAllowed} 
+                            onChange={(e) => dispatch(setDownloadsAllowed(e.target.checked))}
+                        />
+                    }
+                    label="Allow downloads"
+                />
+            </FormGroup>
+
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2, boxShadow: 10 }}
+            >
+                Upload
+            </Button>
+        </Box>
     );
 }
 
--- src/pages/PostUpload/PostUpload.js
+++ src/pages/PostUpload/PostUpload.js
@@ -1,25 +1,95 @@
 import PostUploadForm from "../../components/forms/post-upload/post-upload";
-
-import { Typography, Box } from "@mui/material";
+import PostItem from "../../components/common/post-item/post-item";
+import ImageCropperModal from "../../components/modals/image-cropper-modal/image-cropper-modal";
+import { Stack, Card, Typography, CardContent, Box } from "@mui/material";
+import ImageRightFormContainer from "../../components/containers/image-right-form-container/image-right-form.container";
+import { useDispatch, useSelector } from "react-redux";
+import { setIsShowing as setCropModalIsShowing } from "../../components/modals/image-cropper-modal/imageCropperModalSlice";
+import { setPicture } from "../../components/forms/post-upload/postUploadFormSlice";
+import newPostFormBG from '../../images/bgs/newPostFormBG.png';
 
 
 const PostUpload = (props) => {
-    return (
-        <Box>
-            <Box sx={{pt: 2}}>
-                <Typography gutterBottom variant="h4" component="div" sx={{display: 'flex', justifyContent: 'center', pt: 3, mb: 0}}>
-                    Post upload
-                </Typography>
-                <Typography gutterBottom variant="h6" component="div" sx={{display: 'flex', justifyContent: 'center', pt: 3, mb: 0}}>
-                    Set up your post using form below:
-                </Typography>
-            </Box>
+    const currentUser = useSelector(state => state?.base?.user);
+    const locations = useSelector(state => state?.base?.locations);
+    const isShowing = useSelector(state => state.imageCropperModal.isShowing);
+    const audio = useSelector(state => state.postUploadForm.audio);
+    const title = useSelector(state => state.postUploadForm.title);
+    const description = useSelector(state => state.postUploadForm.description);
+    const picture = useSelector(state => state.postUploadForm.picture);
+    const commentsAllowed = useSelector(state => state.postUploadForm.commentsAllowed);
+    const downloadsAllowed = useSelector(state => state.postUploadForm.downloadsAllowed);
+    const dispatch = useDispatch();
 
-            <Box sx={{pt: 5, m: 1}}>
-                <PostUploadForm/>
-            </Box>
-        </Box>
+
+    const handleImageCropModalClose = (value, picture) => {
+        dispatch(setCropModalIsShowing(value));
+        if (picture !== null) {
+            dispatch(setPicture(picture));
+        }
+    }
+
+
+    return (
+        <ImageRightFormContainer bg={newPostFormBG}>
+            {
+                isShowing
+                ? 
+                <ImageCropperModal 
+                    show={isShowing} 
+                    handleImageCropModalClose={handleImageCropModalClose} 
+                    image={picture} 
+                    what={'post-image'}
+                />
+                :
+                null
+            }
+                <Box sx={{width: '40rem', height: 'fit-content', boxShadow: 0, borderRadius: 5}}>
+                    <Typography gutterBottom variant="h4" component="div" sx={{display: 'flex', justifyContent: 'center', pt: 3, mb: 0}}>
+                        Your new post
+                    </Typography>
+                    <Box sx={{
+                        width: '100%', 
+                        display: 'flex', 
+                        justifyContent: 'center', 
+                        alignItems: 'center', 
+                        mt: 3, 
+                        p: 1,
+                        maxWidth: {xs: '375px', md: '100%'},
+                    }}>
+                        <PostItem
+                            base={{
+                                owner: {
+                                    _id: currentUser?._id,
+                                    nick: currentUser?.nick,
+                                },
+                                ownerAvatar: `${locations?.images}/${currentUser?.avatar}`,
+                                createdAt: 'now',
+                                title,
+                                description,
+                                img: picture,
+                                audio,
+                                likedBy: [],
+                                savedBy: [],
+                                comments: [],
+                            }}
+                            addons={{
+                                commentsAllowed,
+                                downloadsAllowed,
+                                status: "upload",
+                                profileLinkAccessable: false,
+                            }}
+                            id={null}
+                        />
+                    </Box>
+                    <CardContent>
+                        <PostUploadForm/>
+                    </CardContent>
+                </Box>
+        </ImageRightFormContainer>
     );
 }
 
 export default PostUpload;
+
+//<Stack spacing={{ xs: 1, sm: 2 }} sx={{display: 'flex', justifyContent: 'space-around', alignItems: 'center'}} direction="row" useFlexGap flexWrap="wrap">
