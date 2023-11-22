8edce2b95d933494f92ae489f4c6e2a5c19e4fdc
Bohdan <bogdantelep@gmail.com>
2023-11-22 19:03:53 +0300

    code refactoring

--- src/components/common/social-media-login/social-media-login.js
+++ src/components/common/social-media-login/social-media-login.js
@@ -4,16 +4,13 @@ import { FormGroup, Typography, ButtonGroup, Button } from "@mui/material";
 
 
 const SocialMediaLogin = (props) => {
-
-
-
     return (
         <FormGroup>
             <Typography sx={{ my: 1 }}>Sign in using social media</Typography>
             <ButtonGroup sx={{ my: 1, display: 'flex', justifyContent: 'center' }} variant='string' orientation="horizontal">
                 <Button href="http://localhost:8000/api/auth/google"><Google/></Button>
-                <Button href="http://localhost:8000/api/auth/twitter"><Twitter/></Button>
-                <Button><Facebook/></Button>
+                <Button href={process.env.REACT_APP_TWITTER_AUTH}><Twitter/></Button>
+                <Button href={process.env.REACT_APP_FACEBOOK_AUTH}><Facebook/></Button>
             </ButtonGroup>
         </FormGroup>
     );
