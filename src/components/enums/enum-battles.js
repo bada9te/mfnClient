aaf937263a11e46ff7fd2cefcb6efe5bcd1e2a94
bada9te <bogdantelep@gmail.com>
2024-01-12 21:45:24 +0200

    code refactoring

--- src/components/enums/enum-battles.js
+++ src/components/enums/enum-battles.js
@@ -1,22 +1,22 @@
 import BattleItem from "../common/battle-item/battle-item";
 import PostItemUnavailable from "../common/post-item/post-item-unavailable";
 import PostGenerate from "../common/post-item/post-generate";
+import { useReactiveVar } from "@apollo/client";
+import { baseState } from "../baseReactive";
 
 
 const PostFromData = (props) => {
     const { data, battleId, makeBattleVote, votedBy, finished, postNScore } = props;
-
+    const { user: currentUser } = useReactiveVar(baseState);
+ 
     return (
         <>
             {
                 data?._id && data._id !== ""
                 ?
                 <PostGenerate item={data} addonsCorrections={{ 
-                    status: !finished ? "voting" : null,
-                    battleId,
-                    makeBattleVote,
-                    postNScore,
-                    votedBy,
+                    status: !finished && !votedBy.map(i => i._id).includes(currentUser._id) ? "voting" : null,
+                    battleId, makeBattleVote, postNScore, votedBy,
                 }}/>
                 :
                 <PostItemUnavailable/>
