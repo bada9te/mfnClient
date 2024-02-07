8fccd99c371039f0e243a5896a08d67e35d7ce30
bada9te <bogdantelep@gmail.com>
2024-02-07 23:24:04 +0200

    working on chat message

--- src/components/common/chat-message-item/chat-message-item.js
+++ src/components/common/chat-message-item/chat-message-item.js
@@ -1,7 +1,8 @@
 import { useReactiveVar } from "@apollo/client";
-import { Avatar, Box, Button, Paper, Stack, Typography } from "@mui/material";
+import { Avatar, Box, Paper, Stack, Typography } from "@mui/material";
 import { baseState } from "../../baseReactive";
 import getParsedDate from "../../../utils/common-functions/getParsedDate";
+import ChatMessageItemDropDown from "./chat-message-item-dropdown/chat-message-item-dropdown";
 
 
 const ChatMessageItem = props => {
@@ -11,44 +12,28 @@ const ChatMessageItem = props => {
     const avatar = item.owner.avatar.length ? `${locations.images}/${item.owner.avatar}` : "NULL";
 
     return (
+        
         <Stack spacing={1} useFlexGap flexWrap="wrap" flexDirection="row" 
-            sx={{ 
-                width: '100%',
-                display: 'flex',
-                justifyContent: myMsg ? "end": "start",
-                alignItems: 'start',
-            }}
+            sx={{ width: '100%', display: 'flex', justifyContent: myMsg ? "end": "start", alignItems: 'start' }}
         >
-            { !myMsg && <Avatar src={avatar}/> }
+            { !myMsg && <Avatar sx={{ boxShadow: 5 }} src={avatar}/> }
             <Paper elevation={5} sx={{ borderRadius: 3, p: 1, pb: 0 }}>
                 <Stack spacing={0.5} useFlexGap display="flex" alignItems={myMsg ? 'end':'start'}>
-                    <Button sx={{ m: 0, p: 0, display: 'flex', alignItems: 'end', flexWrap: 'wrap', flexDirection: 'column' }}>
-                        <Typography fontSize={12} sx={{px: 0.5, width: 'fit-content', maxWidth: '350px'}}>{`${item.owner.nick}`}</Typography>
-                        <Typography fontSize={9} sx={{px: 0.5, width: 'fit-content', maxWidth: '350px'}} fontStyle='italic'>{`${getParsedDate(new Date(+item.createdAt))}`}</Typography>
-                    </Button>
+                    <ChatMessageItemDropDown>
+                        <Box sx={{ display: 'flex', alignItems: 'end', flexWrap: 'wrap', flexDirection: 'column' }}>
+                            <Typography fontSize={12} sx={{px: 0.5, width: 'fit-content', maxWidth: '350px'}}>{`${item.owner.nick}`}</Typography>
+                            <Typography fontSize={9} sx={{px: 0.5, width: 'fit-content', maxWidth: '350px'}} fontStyle='italic'>{`${getParsedDate(new Date(+item.createdAt))}`}</Typography>
+                        </Box>
+                    </ChatMessageItemDropDown>
                     
-                    <Box 
-                        sx={{ 
-                            width: 'fit-content',
-                            maxWidth: 'calc(100vw - 100px)',
-                            borderRadius: 5,
-                            overflowWrap: 'break-word',
-                            p: 1, px: 1.2,
-                        }} 
-                        elevation={5}
-                    >
-                        <Typography 
-                            fontFamily="'Roboto', sans-serif" 
-                            flexWrap="wrap" 
-                            fontSize={13}
-                            sx={{ maxWidth: '350px'}}
-                        >
+                    <Box sx={{ width: 'fit-content', maxWidth: 'calc(100vw - 100px)', borderRadius: 5, overflowWrap: 'break-word', p: 1, px: 1.2}} elevation={5}>
+                        <Typography fontFamily="'Roboto', sans-serif" flexWrap="wrap" fontSize={13} sx={{ maxWidth: '350px'}}>
                             {item.text}
                         </Typography>
                     </Box>
                 </Stack>
             </Paper>
-            { myMsg && <Avatar src={avatar}/> }
+            { myMsg && <Avatar sx={{ boxShadow: 5 }} src={avatar}/> }
         </Stack>
     );
 }
