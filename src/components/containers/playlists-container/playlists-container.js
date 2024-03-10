267137d3d49b650cbaca754dd5b6d2d0181f7799
bada9te <bogdantelep@gmail.com>
2024-03-10 21:24:13 +0200

    adfhgwauyhgfyiawgfa

--- src/components/containers/playlists-container/playlists-container.js
+++ src/components/containers/playlists-container/playlists-container.js
@@ -170,17 +170,11 @@ const PlaylistsContainer = (props) => {
 
             <TabPanel value={status} index={2}>
                 <ImageRightFormContainer bg={newPlaylistBG} text={t('playlists.create.main_text')}>
-                    <Box sx={{
-                        display: 'flex', 
-                        justifyContent: 'center', 
-                        alignItems: 'center', 
-                        height: 'calc(100vh - 215px)', 
-                        width: '100%',
-                    }}>
+                <Box sx={{width: '30rem', height: 'fit-content', mx: 0, px: 0, boxShadow: 0, borderRadius: 5, mb: {xs: 4, sm: 1, md: 0}}}>
                         {
                             currentUser && currentUser._id !== ""
                             ?
-                            <Box sx={{ width: '30rem', height: 'max-content', boxShadow: 0, borderRadius: 5, background: 'none' }}>
+                            <Box sx={{ width: '30rem', height: 'fit-content', boxShadow: 0, borderRadius: 5, background: 'none', my: 3 }}>
                                 <Typography gutterBottom variant="h4" component="div" sx={{display: 'flex', justifyContent: 'center', pt: 3, mb: 0}}>
                                     {t('playlists.create.title')}
                                 </Typography>
