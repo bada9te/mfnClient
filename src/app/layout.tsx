e5127cd04079d20535fddf0f5336cba45d8ea1f0
bada9te <bogdantelep@gmail.com>
2024-06-29 15:30:34 +0300

    changing scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
                     <div className="grid grid-cols-5 grid-rows-1">
                         <div className="hidden xl:block">
                             <div className="card bg-base-100 shadow-xl w-full main-layout-card rounded-none">
-                                <div className="card-body overflow-y-auto p-4 flex flex-col gap-5">
+                                <div className="card-body overflow-y-auto p-4 flex flex-col gap-5 thin-scrollbar">
                                     {
                                         genres.map((gen, i) => {
 
@@ -48,7 +48,7 @@ export default function RootLayout({
                         </div>
                         <div className="col-span-5 xl:col-span-3">
                             <div className="card bg-base-100 shadow-xl w-full main-layout-card rounded-none">
-                                <div className="card-body overflow-y-auto p-0 gap-0">
+                                <div className="card-body overflow-y-auto p-0 gap-0 thin-scrollbar">
                                     {children}
                                     <Footer/>
                                 </div>
@@ -56,7 +56,7 @@ export default function RootLayout({
                         </div>
                         <div className="hidden xl:block">
                             <div className="card bg-base-100 shadow-xl w-full main-layout-card rounded-none">
-                                <div className="card-body overflow-y-auto p-4">
+                                <div className="card-body overflow-y-auto p-4 thin-scrollbar">
                                     <CurrentTrackRightBar/>
                                 </div>
                             </div>
