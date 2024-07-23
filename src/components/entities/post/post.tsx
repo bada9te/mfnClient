eb15fd5667a2107ea873173ea66f8ae90074e88c
bada9te <bogdantelep@gmail.com>
2024-07-24 01:36:57 +0300

    ui updates

--- src/components/entities/post/post.tsx
+++ src/components/entities/post/post.tsx
@@ -122,7 +122,7 @@ export default function Post(props: {
             <figure><img
                 className="max-h-[180px] w-full min-w-80"
                 src={data?.image ? `${nextConfig.env?.serverFilesEndpoint}/${data?.image}` : '/assets/bgs/profileDefaultBG.png'}
-                alt="Shoes"/>
+                alt="image"/>
             </figure>
 
             <div className="card-body text-start p-5">
@@ -135,17 +135,10 @@ export default function Post(props: {
 
             <div className="stats glass mx-2 mt-2 thin-scrollbar">
                 <div className="stat text-center">
-                    <div className={`cursor-pointer ${data?.likedBy?.find((i: User) => i._id === user?._id) && "text-green-500"}`} onClick={handleSwitchLike}>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            className="inline-block h-8 w-8 stroke-current">
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+                    <div className={`cursor-pointer ${data?.likedBy?.find((i: User) => i._id === user?._id) && "text-red-500"}`} onClick={handleSwitchLike}>
+                        <svg xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 24 24" fill="currentColor" className="inline-block h-8 w-8">
+                            <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
                         </svg>
                     </div>
                     <div className="stat-title">Total Likes</div>
@@ -154,17 +147,10 @@ export default function Post(props: {
 
                 <div className="stat text-center">
                     <div className={`cursor-pointer ${data?.savedBy?.find((i: User) => i._id === user?._id) && "text-yellow-500"}`} onClick={handleSwitchInSaved}>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        className="inline-block h-8 w-8 stroke-current">
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M13 10V3L4 14h7v7l9-11h-7z"></path>
-                    </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 24 24" fill="currentColor" className="inline-block h-8 w-8">
+                            <path fillRule="evenodd" d="M6.32 2.577a49.255 49.255 0 0 1 11.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 0 1-1.085.67L12 18.089l-7.165 3.583A.75.75 0 0 1 3.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93Z" clipRule="evenodd" />
+                        </svg>
                     </div>
                     <div className="stat-title">Total Saves</div>
                     <div className="stat-value text-primary">{data.savedBy?.length}</div>
