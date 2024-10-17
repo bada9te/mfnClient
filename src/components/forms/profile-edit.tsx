e0a6d24a4762ff467fefcc4d6c2b93f49c480a74
bada9te <bogdantelep@gmail.com>
2024-10-18 00:30:50 +0300

    ui updates

--- src/components/forms/profile-edit.tsx
+++ src/components/forms/profile-edit.tsx
@@ -310,7 +310,7 @@ export default function ProfileEditForm(props: {
                             </svg>
                             {userData.user.twitter?.name ? userData.user.twitter.name : dictionary.forms["profile-edit"]["connect-twitter"]}
                         </Link>
-                        <button className="btn btn-error join-item glass hover:bg-red-500 text-white w-[60px]" onClick={handleUnlinkTwitter}>
+                        <button disabled={!userData.user.twitter?.name} className="btn btn-error join-item glass hover:bg-red-500 text-white w-[60px]" onClick={handleUnlinkTwitter}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                                 <path fillRule="evenodd" d="M19.892 4.09a3.75 3.75 0 0 0-5.303 0l-4.5 4.5c-.074.074-.144.15-.21.229l4.965 4.966a3.75 3.75 0 0 0-1.986-4.428.75.75 0 0 1 .646-1.353 5.253 5.253 0 0 1 2.502 6.944l5.515 5.515a.75.75 0 0 1-1.061 1.06l-18-18.001A.75.75 0 0 1 3.521 2.46l5.294 5.295a5.31 5.31 0 0 1 .213-.227l4.5-4.5a5.25 5.25 0 1 1 7.425 7.425l-1.757 1.757a.75.75 0 1 1-1.06-1.06l1.756-1.757a3.75 3.75 0 0 0 0-5.304ZM5.846 11.773a.75.75 0 0 1 0 1.06l-1.757 1.758a3.75 3.75 0 0 0 5.303 5.304l3.129-3.13a.75.75 0 1 1 1.06 1.061l-3.128 3.13a5.25 5.25 0 1 1-7.425-7.426l1.757-1.757a.75.75 0 0 1 1.061 0Zm2.401.26a.75.75 0 0 1 .957.458c.18.512.474.992.885 1.403.31.311.661.555 1.035.733a.75.75 0 0 1-.647 1.354 5.244 5.244 0 0 1-1.449-1.026 5.232 5.232 0 0 1-1.24-1.965.75.75 0 0 1 .46-.957Z" clipRule="evenodd" />
                             </svg>
@@ -318,7 +318,7 @@ export default function ProfileEditForm(props: {
                     </div>
                     
                     <div className="join join-horizontal w-full">
-                        <Link href={envCfg.googleAuthURL as string} className={`btn w-[calc(100%-60px)] join-item glass hover:bg-white text-white hover:text-black ${userData.user.google?.email && 'bg-white/80 text-black'}`}>
+                        <Link href={envCfg.googleAuthURL as string} className={`btn w-[calc(100%-60px)] join-item glass hover:bg-white hover:text-black ${userData.user.google?.email ? 'bg-white/80 text-black' : 'text-white hover:text-black'}`}>
                             <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
                                 <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/>
                                 <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/>
@@ -328,7 +328,7 @@ export default function ProfileEditForm(props: {
                             </svg>
                             {userData.user.google?.email ? userData.user.google?.email : dictionary.forms["profile-edit"]["connect-google"]}
                         </Link>
-                        <button className="btn btn-error join-item glass hover:bg-red-500 text-white w-[60px]" onClick={handleUnlinkGoogle}>
+                        <button disabled={!userData.user.google?.email} className="btn btn-error join-item glass hover:bg-red-500 text-white w-[60px]" onClick={handleUnlinkGoogle}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                                 <path fillRule="evenodd" d="M19.892 4.09a3.75 3.75 0 0 0-5.303 0l-4.5 4.5c-.074.074-.144.15-.21.229l4.965 4.966a3.75 3.75 0 0 0-1.986-4.428.75.75 0 0 1 .646-1.353 5.253 5.253 0 0 1 2.502 6.944l5.515 5.515a.75.75 0 0 1-1.061 1.06l-18-18.001A.75.75 0 0 1 3.521 2.46l5.294 5.295a5.31 5.31 0 0 1 .213-.227l4.5-4.5a5.25 5.25 0 1 1 7.425 7.425l-1.757 1.757a.75.75 0 1 1-1.06-1.06l1.756-1.757a3.75 3.75 0 0 0 0-5.304ZM5.846 11.773a.75.75 0 0 1 0 1.06l-1.757 1.758a3.75 3.75 0 0 0 5.303 5.304l3.129-3.13a.75.75 0 1 1 1.06 1.061l-3.128 3.13a5.25 5.25 0 1 1-7.425-7.426l1.757-1.757a.75.75 0 0 1 1.061 0Zm2.401.26a.75.75 0 0 1 .957.458c.18.512.474.992.885 1.403.31.311.661.555 1.035.733a.75.75 0 0 1-.647 1.354 5.244 5.244 0 0 1-1.449-1.026 5.232 5.232 0 0 1-1.24-1.965.75.75 0 0 1 .46-.957Z" clipRule="evenodd" />
                             </svg>
@@ -336,7 +336,7 @@ export default function ProfileEditForm(props: {
                     </div>
 
                     <div className="join join-horizontal w-full">
-                        <Link href={envCfg.facebookAuthURL as string} className={`btn w-[calc(100%-60px)] join-item glass text-white hover:bg-blue-500 ${userData.user.facebook?.name && 'bg-blue-500'}`}>
+                        <Link tabIndex={undefined} href={envCfg.facebookAuthURL as string} className={`btn w-[calc(100%-60px)] join-item glass text-white hover:bg-blue-500 ${userData.user.facebook?.name && 'bg-blue-500'}`}>
                             <svg fill="#0091ff" height="25px" width="25px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 310 310" stroke="#0091ff">
                             <g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
                             <g id="SVGRepo_iconCarrier"> 
@@ -348,7 +348,7 @@ export default function ProfileEditForm(props: {
                             </svg>
                             { userData.user.facebook?.name ? userData.user.facebook?.name : dictionary.forms["profile-edit"]["connect-facebook"] }
                         </Link>
-                        <button className="btn btn-error join-item glass hover:bg-red-500 text-white w-[60px]" onClick={handleUnlinkFacebook}>
+                        <button disabled={!userData.user.facebook?.name} className="btn btn-error join-item glass hover:bg-red-500 text-white w-[60px]" onClick={handleUnlinkFacebook}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                                 <path fillRule="evenodd" d="M19.892 4.09a3.75 3.75 0 0 0-5.303 0l-4.5 4.5c-.074.074-.144.15-.21.229l4.965 4.966a3.75 3.75 0 0 0-1.986-4.428.75.75 0 0 1 .646-1.353 5.253 5.253 0 0 1 2.502 6.944l5.515 5.515a.75.75 0 0 1-1.061 1.06l-18-18.001A.75.75 0 0 1 3.521 2.46l5.294 5.295a5.31 5.31 0 0 1 .213-.227l4.5-4.5a5.25 5.25 0 1 1 7.425 7.425l-1.757 1.757a.75.75 0 1 1-1.06-1.06l1.756-1.757a3.75 3.75 0 0 0 0-5.304ZM5.846 11.773a.75.75 0 0 1 0 1.06l-1.757 1.758a3.75 3.75 0 0 0 5.303 5.304l3.129-3.13a.75.75 0 1 1 1.06 1.061l-3.128 3.13a5.25 5.25 0 1 1-7.425-7.426l1.757-1.757a.75.75 0 0 1 1.061 0Zm2.401.26a.75.75 0 0 1 .957.458c.18.512.474.992.885 1.403.31.311.661.555 1.035.733a.75.75 0 0 1-.647 1.354 5.244 5.244 0 0 1-1.449-1.026 5.232 5.232 0 0 1-1.24-1.965.75.75 0 0 1 .46-.957Z" clipRule="evenodd" />
                             </svg>
