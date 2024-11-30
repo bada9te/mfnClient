3208a27f32d6c61a8cce6e993d5054f303805977
bada9te <bogdantelep@gmail.com>
2024-11-30 23:56:46 +0200

    config updates

--- src/components/entities/achievement/achievement.tsx
+++ src/components/entities/achievement/achievement.tsx
@@ -1,4 +1,5 @@
 import { Achievement as TAchievement } from "@/utils/graphql-requests/generated/schema";
+import { CheckCheck } from "lucide-react";
 
 const getColor = (rarity: string) => {
     switch (rarity) {
@@ -19,9 +20,7 @@ export default function Achievement(props: {data: TAchievement, isCompleted: boo
             {
                 isCompleted
                 &&
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-8 absolute right-3 top-3">
-                    <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
-                </svg>
+                <CheckCheck />
             }
 
             <div className="card-body items-center text-center">
