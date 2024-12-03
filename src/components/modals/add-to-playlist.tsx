07179f3508b8d20eec42c81ef4a5c904caa1d2b3
bada9te <bogdantelep@gmail.com>
2024-12-04 00:23:07 +0200

    playlists modal updates

--- src/components/modals/add-to-playlist.tsx
+++ src/components/modals/add-to-playlist.tsx
@@ -4,9 +4,10 @@ import { getDictionary } from "@/dictionaries/dictionaries";
 import MainButton from "../common/main-button/main-button";
 import { ChevronLeft, ChevronRight, PlusIcon, X } from "lucide-react";
 import { useAppSelector } from "@/lib/redux/store";
-import {  usePlaylistsByOwnerIdLazyQuery } from "@/utils/graphql-requests/generated/schema";
+import {  usePlaylistsByOwnerIdLazyQuery, usePlaylistsSwitchTrackMutation } from "@/utils/graphql-requests/generated/schema";
 import getTimeSince from "@/utils/common-functions/getTimeSince";
 import Link from "next/link";
+import { useSnackbar } from "notistack";
 
 const LIMIT = 6;
 
@@ -24,7 +25,9 @@ export default function AddToPlaylistModal({
     const [page, setPage] = useState(1);
     const [selectedPlaylists, setSelectedPlaylists] = useState<string[]>([]);
 
-    const [getPlaylistsByOwner, { data, loading }] = usePlaylistsByOwnerIdLazyQuery();
+    const [getPlaylistsByOwner, { data, loading, refetch }] = usePlaylistsByOwnerIdLazyQuery();
+    const [switchTrackInPlaylists] = usePlaylistsSwitchTrackMutation();
+    const {enqueueSnackbar} = useSnackbar();
 
 
     const handleOpen = () => {
@@ -62,6 +65,25 @@ export default function AddToPlaylistModal({
             setSelectedPlaylists(selectedPlaylists.filter(i => i !== playlistId));
         }
     }
+
+    const handleSwitchTracks = () => {
+        onClose();
+        enqueueSnackbar("Switching tracks...", { autoHideDuration: 2000 });
+        switchTrackInPlaylists({
+            variables: {
+                input: {
+                    playlistsIds: selectedPlaylists,
+                    trackId: postId,
+                } 
+            }
+        }).then(() => {
+            enqueueSnackbar("Done", { autoHideDuration: 2500, variant: 'success' });
+            setSelectedPlaylists([]);
+            refetch();
+        }).catch(() => {
+            enqueueSnackbar("Sth went wrong, pls try again later", { autoHideDuration: 3000, variant: 'error' });
+        });
+    }
     
     return (
         <>
@@ -81,69 +103,72 @@ export default function AddToPlaylistModal({
                     </form>
 
                     <h4 className="font-bold text-lg">Add to playlist</h4>
+                    {
+                        data?.playlistsByOwnerId.count 
+                        ?
+                        <>
+                            <div className="overflow-auto max-h-[300px] thin-scrollbar bg-base-300 rounded-xl mt-7">
+                                <table className="table table-zebra">
+                                    {/* head */}
+                                    <thead>
+                                        <tr>
+                                            <th>Switch in</th>
+                                            <th>Title</th>
+                                            <th>Amount</th>
+                                            <th>Since creation</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {/* row x */}
+                                        {
+                                            data?.playlistsByOwnerId.playlists?.map((i, key) => {
+                                                return (
+                                                    <tr key={key}>
+                                                        <th className="flex flex-row gap-2 items-center justify-start">
+                                                            <label>
+                                                                <input 
+                                                                    type="checkbox" 
+                                                                    className="checkbox checkbox-sm" 
+                                                                    onChange={(e) => handlePlaylistSelect(e, i._id)}
+                                                                    checked={selectedPlaylists.includes(i._id)}
+                                                                    readOnly
+                                                                />
+                                                            </label>
+                                                            <div className={`h-2 w-2 ${i.tracks?.map(j => j._id).includes(postId) ? 'bg-green-500' : 'bg-gray-600'} rounded-full`}></div>
+                                                        </th>
+                                                        <td>{i.title}</td>
+                                                        <td>{i.tracks?.length || 0}</td>
+                                                        <td>{getTimeSince(new Date(+i.createdAt))}</td>
+                                                    </tr>
+                                                );
+                                            })
+                                        }
+                                    </tbody>
+                                </table>
 
-                    <div className="overflow-auto max-h-[300px] thin-scrollbar bg-base-300 rounded-xl mt-7">
-                        <table className="table table-zebra">
-                            {/* head */}
-                            <thead>
-                                <tr>
-                                    <th>Switch in</th>
-                                    <th>Title</th>
-                                    <th>Amount</th>
-                                    <th>Since creation</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {/* row x */}
-                                {
-                                    data?.playlistsByOwnerId.count 
-                                    &&
-                                    data?.playlistsByOwnerId.playlists?.map((i, key) => {
-                                        return (
-                                            <tr key={key}>
-                                                <th>
-                                                    <label>
-                                                        <input 
-                                                            type="checkbox" 
-                                                            className="checkbox checkbox-sm" 
-                                                            onChange={(e) => handlePlaylistSelect(e, i._id)}
-                                                            checked={selectedPlaylists.includes(i._id)}
-                                                            readOnly
-                                                        />
-                                                    </label>
-                                                </th>
-                                                <td>{i.title}</td>
-                                                <td>{i.tracks?.length || 0}</td>
-                                                <td>{getTimeSince(new Date(+i.createdAt))}</td>
-                                            </tr>
-                                        );
-                                    })
-                                }
-                            </tbody>
-                        </table>
-
-                        <div className="flex flex-row gap-1 text-lg p-3 items-center justify-center">
-                            <ChevronLeft className="cursor-pointer" onClick={() => handlePageChange(page - 1)} />
-                            {page}/{data?.playlistsByOwnerId ? (Math.ceil(data.playlistsByOwnerId.count / LIMIT)) : 1}
-                            <ChevronRight className="cursor-pointer" onClick={() => handlePageChange(page + 1)} />
+                                <div className="flex flex-row gap-1 text-lg p-3 items-center justify-center">
+                                    <ChevronLeft className="cursor-pointer" onClick={() => handlePageChange(page - 1)} />
+                                    {page}/{data?.playlistsByOwnerId ? (Math.ceil(data.playlistsByOwnerId.count / LIMIT)) : 1}
+                                    <ChevronRight className="cursor-pointer" onClick={() => handlePageChange(page + 1)} />
+                                </div>
+                            </div>
+                            <div className="modal-action z-50"> 
+                                <MainButton
+                                    handler={selectedPlaylists.length ? handleSwitchTracks : () => {}}
+                                    color="primary"
+                                >
+                                    <PlusIcon className="mr-1"/>
+                                    Switch in {selectedPlaylists.length} playlists
+                                </MainButton>
+                            </div>
+                        </>
+                        :
+                        <div className="flex flex-col items-center justify-center">
+                            <Link href={"/playlists/create"}>Create new playlist</Link>
                         </div>
-                    </div>
-
-                    <div className="modal-action z-50"> 
-                        <MainButton
-                            handler={onClose}
-                            color="primary"
-                        >
-                            <PlusIcon className="mr-1"/>
-                            Add to {selectedPlaylists.length} playlists
-                        </MainButton>
-                    </div>
+                    }
                 </div>
-
             </dialog>
         </>
     );
 }
-
-
-// <Link href={"/playlists/create"}>Create new</Link>
