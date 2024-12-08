02e7193e2446171756ecabbc681390243e550817
bada9te <bogdantelep@gmail.com>
2024-12-08 19:15:54 +0200

    ui updates

--- src/components/modals/cookie-info-modal.tsx
+++ src/components/modals/cookie-info-modal.tsx
@@ -31,13 +31,13 @@ export default function WeAreUsingCookiesModal() {
                 translate-x-[50%] md:translate-x-0 -left-1/2 
             "
         >
-            <div className="flex flex-row gap-4 bg-base-300 bg-opacity-65 backdrop-blur-xl rounded-xl shadow-lg p-3">
+            <div className="flex flex-row gap-4 bg-base-300 bg-opacity-65 backdrop-blur-xl rounded-xl shadow-lg p-3 relative">
                 <Image src={"/assets/icons/cookie.png"} alt="cookie" width={400} height={400} className="w-24 md:w-32 h-24 md:h-32"/>
                 <div className="w-full h-full text-white">
-                    <span className="text-lg font-bold">We use cookies to ensure you get the best experience on our website.</span>
+                    <span className="text-md font-bold">We use only essential cookies to provide you with the best possible experience on our website.</span>
                     <br/>
-                    <span className="text-sm">Authentication cookies are necessary for secure logins.</span>
-                    <div className="w-full flex justify-end p-2 pt-3">
+                    <p className="text-sm max-w-72 mt-1">Authentication cookies are necessary for secure logins.</p>
+                    <div className="m-2 md:m-0 block md:absolute bottom-5 right-5 justify-self-end">
                         <MainButton
                             handler={handleCookieOk}
                             color="primary"
