1796a182fde3dc055046c6ac078c7a98f6cd740a
bada9te <bogdantelep@gmail.com>
2024-12-09 23:00:47 +0200

    ui updates

--- src/components/modals/cookie-info-modal.tsx
+++ src/components/modals/cookie-info-modal.tsx
@@ -37,7 +37,7 @@ export default function WeAreUsingCookiesModal() {
                     <span className="text-md font-bold">We use only essential cookies to provide you with the best possible experience on our website.</span>
                     <br/>
                     <p className="text-sm max-w-72 mt-1">Authentication cookies are necessary for secure logins.</p>
-                    <div className="m-2 md:m-0 block md:absolute bottom-5 right-5 justify-self-end">
+                    <div className="m-2 md:m-0 block md:absolute bottom-5 right-5 justify-self-end self-end">
                         <MainButton
                             handler={handleCookieOk}
                             color="primary"
